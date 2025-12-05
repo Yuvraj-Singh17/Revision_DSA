444752f4effedcc9f3dae02fbdff10e55e341b8d
Yuvraj <yuvivishu17@gmail.com>
2025-12-05 19:57:36 +0530

    Array 1

---
+++ Array/ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] h) {
+       int l = 0;
+       int r = h.length-1;
+       int max = 0;
+
+       while(l < r){
+        int area = Math.min(h[l] , h[r]) * (r-l);
+        max = Math.max(max  ,area);
+        if(h[l] < h[r]) l++;
+        else r--;
+       }
+       return max;
+    }
+}
