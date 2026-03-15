2efd51913df880e9e80578acfdfcc6ef02ca730c
Yuvraj <yuvivishu17@gmail.com>
2026-03-16 00:10:53 +0530

    Added sum of Array

---
+++ DSA/Recursion/SumOfArray.java
@@ -0,0 +1,13 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        System.out.println(sum(arr , 0));
+    }
+    public static int sum(int[] arr , int n){
+        if(n == arr.length){
+            return 0;
+        }
+        return arr[n] + sum(arr , n+1);
+    }
+}
